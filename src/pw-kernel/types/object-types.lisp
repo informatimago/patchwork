231db6280edc18d3328cf74362ad6c34b6c31743
pjb <pjb>
2012-04-11 22:26:49 +0000

    Initial check-in of PatchWork sources.

---
+++ src/pw-kernel/types/object-types.lisp
@@ -0,0 +1,1 @@
+;;;;=========================================================;;;;;;;;  PATCH-WORK;;;;  By Mikael Laurson, Jacques Duthen, Camilo Rueda.;;;;  © 1986-1992 IRCAM ;;;;;;;;=========================================================(in-package :PW)(push-to-object-types 'chord)(push-to-object-types 'collector)(push-to-object-types 'bpf)(push-to-object-types 'points-view)
