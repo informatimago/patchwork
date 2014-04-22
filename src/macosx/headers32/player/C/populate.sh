ec0516f8c5293ec3e7ccdb93f946b43e50c38b16
Pascal J. Bourguignon <pjb@informatimago.com>
2014-04-22 05:36:42 +0200

    Added player populate.sh scripts.

---
+++ src/macosx/headers32/player/C/populate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ "$(basename "$(pwd)")" = C -a "$(basename "$(dirname "$(pwd)")")" = player ] ; then
+    rm -rf Player
+    CFLAGS="-m32 -msse2 -fobjc-abi-version=2 -isysroot ${SDK} -mmacosx-version-min=10.5"
+    export CFLAGS
+    h-to-ffi.sh -D__Types__ -I/Library/Frameworks/MidiShare.framework/Headers/ /Library/Frameworks/Player.framework/Headers/Player.h
+else
+    echo "Please   cd player/C   before running   sh ./populate.sh"
+fi
+
+
+
