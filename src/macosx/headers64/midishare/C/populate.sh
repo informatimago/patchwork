3f00d1491cff7047ba73f0d50496b55cbfdd366b
Pascal J. Bourguignon <pjb@informatimago.com>
2014-04-25 04:45:12 +0200

    Corrected case of MidiShare.framework.

--- src/macosx/headers64/midishare/C/populate.sh
+++ src/macosx/headers64/midishare/C/populate.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ "$(basename "$(pwd)")" = C -a "$(basename "$(dirname "$(pwd)")")" = midishare ] ; then
-    rm -rf Midishare
+    rm -rf MidiShare
     CFLAGS="-m64 -msse2 -fobjc-abi-version=2 -isysroot ${SDK} -mmacosx-version-min=10.5"
     export CFLAGS
-    h-to-ffi.sh -D__Types__ /Library/Frameworks/Midishare.framework/Headers/MidiShare.h
-    # h-to-ffi.sh -D__Types__ -I/Library/Frameworks/Midishare.framework/Headers/ /Library/Frameworks/Player.framework/Headers/Player.h
+    h-to-ffi.sh -D__Types__ /Library/Frameworks/MidiShare.framework/Headers/MidiShare.h
+    # h-to-ffi.sh -D__Types__ -I/Library/Frameworks/MidiShare.framework/Headers/ /Library/Frameworks/Player.framework/Headers/Player.h
 else
     echo "Please   cd midishare/C   before running   sh ./populate.sh"
 fi
